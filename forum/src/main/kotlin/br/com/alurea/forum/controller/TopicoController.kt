2aec261db2e691f2276f92a29140ee96dd0867c3
hugol <hugoleo3129@gmail.com>
2023-06-07 14:51:15 -0300

    criando classe Service

--- forum/src/main/kotlin/br/com/alurea/forum/controller/TopicoController.kt
+++ forum/src/main/kotlin/br/com/alurea/forum/controller/TopicoController.kt
@@ -3,6 +3,7 @@ package br.com.alurea.forum.controller
 import br.com.alurea.forum.model.Curso
 import br.com.alurea.forum.model.Topico
 import br.com.alurea.forum.model.Usuario
+import br.com.alurea.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,27 +11,11 @@ import java.util.*
 
 @RestController
 @RequestMapping("/topicos")
-class TopicoController {
+class TopicoController(private val service: TopicoService) {
 
     @GetMapping
     fun listar(): List<Topico> {
-        val topico = Topico(
-            id = 1,
-            titulo = "Duvida Kotlin",
-            mensagem = "Variaveis no Kotlin",
-            curso = Curso (
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programacao"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome= "Ana da Silva",
-                email= "ana@email.com"
-            )
-
-        )
-        return Arrays.asList(topico, topico,topico)
+        return service.listar()
     }
 
 }
