8d2e39e558c9ea44316d0fe9f409ff022d994a3b
hugol <hugoleo3129@gmail.com>
2023-06-07 15:25:38 -0300

    segundo método end points{id}

--- forum/src/main/kotlin/br/com/alurea/forum/controller/TopicoController.kt
+++ forum/src/main/kotlin/br/com/alurea/forum/controller/TopicoController.kt
@@ -5,6 +5,7 @@ import br.com.alurea.forum.model.Topico
 import br.com.alurea.forum.model.Usuario
 import br.com.alurea.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -16,6 +17,11 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping
     fun listar(): List<Topico> {
         return service.listar()
+    }
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): Topico{
+        return service.buscarPorId(id)
     }
 
 }
